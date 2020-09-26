5a532a5cbae0a9b2c0f938d31421d9c95921e672
junaid404 <dev.junaidjaved@gmail.com>
2020-09-25 23:43:35 -0700

    fix: covert top followers image on feed page to a link

--- client/src/components/feed/Following.jsx
+++ client/src/components/feed/Following.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import { useFeedPageStyles } from "../../helpers/styles";
+import { Link } from "react-router-dom";
 
 const Followings = ({ data }) => {
   const classes = useFeedPageStyles();
   return (
     <div className={classes.followings}>
-      <img
-        className={classes.followingImg}
-        src={data.profile_image}
-        alt="profile"
-      />
+      <Link to={`/${data.username}`}>
+        <img
+          className={classes.followingImg}
+          src={data.profile_image}
+          alt="profile"
+        />
+      </Link>
       <Typography
         className={classes.followingText}
         component="span"
